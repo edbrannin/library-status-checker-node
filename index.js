120c0df8a4467462fe6811cce154ff9dc922e78f
Ed Brannin <edbrannin@gmail.com>
2017-10-04 06:17:32 -0400

    await page.waitFor....() and better logging

--- index.js
+++ index.js
@@ -7,64 +7,84 @@ const loadConfig = async () => {
 const IGNORE_AJAX = [
   "branch/branchLimits?",
   ".png",
+  "tationLocation/thisBranch",
+  "ssl.google-analytics.com",
+  "ocationConfiguration/getIfChanged",
+  "/tmpl/",
 ]
 
 const getAccountInfo = async (number, lastName) => {
   try {
-  console.log("Getting browser.");
-  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    console.log("Getting browser.");
+    const browser = await puppeteer.launch({
+      //args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
 
-  const page = await browser.newPage();
+    const page = await browser.newPage();
 
-  console.log("Going to page");
-  await page.goto('https://catalogplus.libraryweb.org/?section=myAccount');
+    console.log("Going to page");
+    await page.goto('https://catalogplus.libraryweb.org/?section=myAccount');
 
-  page.waitForSelector('.innerLoginForm #barcode', { visible: true });
-  await page.focus('.innerLoginForm #barcode');
-  await page.type(number);
-  await page.focus('.innerLoginForm #lastName');
-  await page.type(lastName);
-  await page.screenshot({path: `login-${number}.png`});
+    page.waitForSelector('.innerLoginForm #barcode', { visible: true });
+    await page.focus('.innerLoginForm #barcode');
+    await page.type(number);
+    await page.focus('.innerLoginForm #lastName');
+    await page.type(lastName);
+    await page.screenshot({path: `login-${number}.png`});
 
-  page.on('response', async response => {
-    const url = response.request().url;
-    for (var i = 0; i < IGNORE_AJAX.length; i += 1) {
-      // Supposedly faster than forEach
-      if (url.includes(IGNORE_AJAX[i])) {
-        return;
+    page.on('response', async response => {
+      const url = response.request().url;
+      for (var i = 0; i < IGNORE_AJAX.length; i += 1) {
+        // Supposedly faster than forEach
+        if (url.includes(IGNORE_AJAX[i])) {
+          return;
+        }
       }
-    }
-    console.log();
-    console.log("==========");
-    console.log(`Got response from ${url}`);
-    // console.log(await response.text());
-    try {
-      console.log(await response.json());
+      console.log();
+      console.log("==========");
+      console.log(`Got response from ${url}`);
+      console.log("Headers:", response.headers);
+      try {
+        if (false) {
+          console.log("Text");
+          console.log(await response.text());
+        }
+        console.log("JSON");
+        console.log(await response.json());
+      } catch (e) {
+        console.error(`Error trying to log JSON from ${url}`, e);
+      }
+    });
+
+    await page.waitForNavigation({ waitUntil: 'networkidle' });
+
+    await page.waitForSelector('#submitLoginFormButton', { visible: true });
+    await page.click('#submitLoginFormButton');
+    await page.screenshot({path: `logged-in-${number}.png`});
+
+    try{
+      await page.waitForSelector('#accountActivity_tab', { visible: true });
+      await page.waitFor(1000);
+      await page.click('#accountActivity_tab');
     } catch (e) {
-      console.error(`Error trying to log JSON: ${e}`);
+      console.error("Error waiting for account activity", e);
+      await page.screenshot({path: `login-error-${number}.png`});
     }
-  });
-
-  page.waitForSelector('#submitLoginFormButton', { visible: true });
-  await page.click('#submitLoginFormButton');
-  await page.screenshot({path: `logged-in-${number}.png`});
-
-  try{
-    page.waitForSelector('#accountActivity_tab', { visible: true });
-    await page.click('#accountActivity_tab');
-  } catch (e) {
-    console.error("Error waiting for account activity", e);
-    await page.screenshot({path: `login-error-${number}.png`});
-  }
 
 
-  // console.log(await page.content());
-  await page.screenshot({path: 'screenshot.png'});
+    // console.log(await page.content());
+    await page.screenshot({path: 'screenshot.png'});
 
-  await page.close();
-  await browser.close();
+
+    console.log("Waiting 10 seconds to wrap up....");
+    await page.waitFor(10000);
+
+    console.log("Cleaning up.");
+    await page.close();
+    await browser.close();
   } catch (e) {
     console.error("Error getting loans", e);
+    console.error("Stack:", e.stack);
   }
 }
 
